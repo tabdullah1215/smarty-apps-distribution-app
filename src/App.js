5c8e53c6b070a2625719bc5f49b90d2adc201ac6
Tariq Abdullah <tabdullah1215@gmail.com>
2024-10-01 23:20:05 -0400

    upgrade app.js to v6

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import OwnerDashboard from './components/OwnerDashboard';
 import DistributorDashboard from './components/DistributorDashboard';
 import DistributorRegistration from './components/DistributorRegistration';
 
 function App() {
-  return (
-      <Router>
-        <Switch>
-          <Route path="/" exact component={OwnerDashboard} />
-          <Route path="/distributor" exact component={DistributorDashboard} />
-          <Route path="/register/:token" component={DistributorRegistration} />
-        </Switch>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<OwnerDashboard />} />
+                <Route path="/distributor" element={<DistributorDashboard />} />
+                <Route path="/register/:token" element={<DistributorRegistration />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
