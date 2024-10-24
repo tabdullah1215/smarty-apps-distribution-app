3e2d755d7a809a3e4e5533460c075bd3f6e9b688
Tariq Abdullah <tabdullah1215@gmail.com>
2024-10-23 23:49:39 -0400

    fix mobile insert orders button

--- src/components/OwnerDashboard.js
+++ src/components/OwnerDashboard.js
@@ -323,16 +323,17 @@ export default function OwnerDashboard() {
 
                 <div className="mt-8 bg-white rounded-lg shadow-md p-6">
                     <h2 className="text-xl font-semibold mb-4">Insert Order Number</h2>
-                    <form onSubmit={insertOrderNumber} className="flex items-center">
+                    <form onSubmit={insertOrderNumber}
+                          className="flex flex-col sm:flex-row items-stretch sm:items-center gap-2 sm:gap-0">
                         <input
                             type="text"
                             value={orderNumber}
                             onChange={(e) => setOrderNumber(e.target.value)}
                             placeholder="Enter order number"
-                            className="flex-grow p-2 border rounded-l"
+                            className="flex-grow p-2 border rounded sm:rounded-r-none"
                             required
                         />
-                        <button type="submit" className="py-2 px-4 bg-blue-500 text-white rounded-r">
+                        <button type="submit" className="py-2 px-4 bg-blue-500 text-white rounded sm:rounded-l-none">
                             Insert
                         </button>
                     </form>
