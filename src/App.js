6f0965a36d7814808bc2db698b35932a9b6e3362
Tariq Abdullah <tabdullah1215@gmail.com>
2024-11-02 03:52:35 -0400

    add pre-jwt authentication

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import OwnerDashboard from './components/OwnerDashboard';
 import DistributorDashboard from './components/DistributorDashboard';
 import DistributorRegistration from './components/DistributorRegistration';
+import Login from './components/Login';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<OwnerDashboard />} />
                 <Route path="/distributor" element={<DistributorDashboard />} />
                 <Route path="/register/:linkType/:token" element={<DistributorRegistration />} />
+                <Route path="/login" element={<Login />} />  {/* Add this route */}
             </Routes>
         </Router>
     );
